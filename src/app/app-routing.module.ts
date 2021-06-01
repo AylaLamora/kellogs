b1ca5ecf35b8839bcbb42305357f3092ceea5ef1
Melvaz <melissavazquezunzueta@gmail.com>
2021-06-01 11:51:49 -0600

    Navbar Jumbotron

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
 
+  {path: 'principal',component: PrincipalComponent},
+  {path: '', pathMatch:'full', redirectTo:'principal' },
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
